a7c4c5aebc824debe297b970acd21ae6bcdc1dfd
Scott Morse <scottmorsedev@gmail.com>
2018-09-07 17:47:57 -0500

    First draft complete

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const codeImg = document.querySelector('#code-img');
 const jazzP = document.querySelector('#jazz');
 const musImg = document.querySelector('#mus-img');
 const simpleNav = document.querySelector('#simple-nav');
+const projectCircles = Array.from(document.querySelectorAll('.project-circle'));
+const projectLinks = Array.from(document.querySelectorAll('.project-link'));
 
 function bounceLogo(){
     [logoImg1,circle1].map(el => el.classList.add('load'));
@@ -34,4 +36,39 @@ function bounceLogo(){
     });
 }
 
+const circleTrans = 'width .5s ease, height .5s ease, opacity .5s ease,border-color .3s ease';
+
+function circleHandle() {
+    activeCircle = document.querySelector(`#project${this.id[7]}-circle`);
+    activeCircle.style.borderColor = "var(--red)";
+    activeCircle.style.transition = circleTrans;
+    activeCircle.style.width = activeCircle.style.height = "calc(var(--project-pic-w) + 3vw)";
+    activeCircle.style.filter = "blur(3px)";
+    activeCircle.style.opacity = 0;
+}
+
+function circleColor() {
+    activeCircle = document.querySelector(`#project${this.id[7]}-circle`);
+    activeCircle.style.borderColor = "var(--light-blue)";
+}
+
+projectCircles.forEach(circle => {
+    circle.addEventListener('transitionend',()=> {
+        circle.style.transition = 'border-color .3s ease';
+        circle.style.width = circle.style.height = "var(--project-pic-w)";
+        circle.style.filter = "blur(0)";
+        circle.style.opacity = "1";
+        setTimeout(()=>{
+            circle.style.transition = circleTrans;
+        },10);
+    });
+})
+
+function protectTransition(){
+    projectCircles.forEach(circle => circle.style.transition = 'none');
+}
+
 window.addEventListener('load',bounceLogo);
+window.addEventListener('resize',protectTransition);
+projectLinks.forEach(link => link.addEventListener('mouseover',circleHandle));
+projectLinks.forEach(link => link.addEventListener('mouseout',circleColor));
